b03837e72dcb6e2f7201b0d44051c39d05dd3231
Allyjuke96 <claudia.melistonti@gmail.com>
2021-06-02 11:12:22 +0200

    Init

--- app/src/main/java/com/example/hri/MainActivity.java
+++ app/src/main/java/com/example/hri/MainActivity.java
@@ -2,21 +2,30 @@ package com.example.hri;
 
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.ListenBuilder;
+import com.aldebaran.qi.sdk.builder.PhraseSetBuilder;
 import com.aldebaran.qi.sdk.builder.SayBuilder;
 import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.conversation.Listen;
+import com.aldebaran.qi.sdk.object.conversation.ListenResult;
+import com.aldebaran.qi.sdk.object.conversation.PhraseSet;
 import com.aldebaran.qi.sdk.object.conversation.Say;
+import com.aldebaran.qi.sdk.util.PhraseSetUtil;
+
 
 public class MainActivity extends RobotActivity implements RobotLifecycleCallbacks {
-
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Register the RobotLifecycleCallbacks to this Activity.
         QiSDK.register(this, this);
+
     }
 
     @Override
@@ -29,11 +38,39 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
     @Override
     public void onRobotFocusGained(QiContext qiContext) {
         // The robot focus is gained.
-        Say say = SayBuilder.with(qiContext) // Create the builder with the context.
-                .withText("Hello human!!") // Set the text to say.
-                .build(); // Build the say action.
-        // Execute the action.
-        say.run();
+        //Say say = SayBuilder.with(qiContext) // Create the builder with the context.
+        //        .withText("Hello human!!") // Set the text to say.
+        //        .build(); // Build the say action.
+        //// Execute the action.
+        //say.run();
+        PhraseSet phraseSet = PhraseSetBuilder.with(qiContext)
+                .withTexts("Hello", "Hi")
+                .build();
+
+
+
+        // Create the PhraseSet 1.
+        PhraseSet phraseSetYes = PhraseSetBuilder.with(qiContext) // Create the builder using the QiContext.
+                .withTexts("yes", "OK", "alright", "let's do this") // Add the phrases Pepper will listen to.
+                .build(); // Build the PhraseSet.
+
+        // Create the PhraseSet 2.
+        PhraseSet phraseSetNo = PhraseSetBuilder.with(qiContext) // Create the builder using the QiContext.
+                .withTexts("no", "Sorry", "I can't") // Add the phrases Pepper will listen to.
+                .build(); // Build the PhraseSet.
+
+        Listen listen = ListenBuilder.with(qiContext)
+                .withPhraseSets(phraseSetYes, phraseSetNo)
+                .build();
+        ListenResult listenResult = listen.run();
+
+        Log.i(TAG, "Heard phrase: " + listenResult.getHeardPhrase().getText()); // Prints "Heard phrase: forwards".
+        PhraseSet matchedPhraseSet = listenResult.getMatchedPhraseSet();
+        if (PhraseSetUtil.equals(matchedPhraseSet, phraseSetYes)) {
+            Log.i(TAG, "Heard phrase set: yes");
+        } else if (PhraseSetUtil.equals(matchedPhraseSet, phraseSetNo)) {
+            Log.i(TAG, "Heard phrase set: no");
+        }
     }
 
     @Override
